50b9c28cd174ecf7fd13e118887726bdd622485a
Deepakbaria <deepak.bariya@techiegent.com>
2023-03-04 12:05:02 +0530

    first

---
+++ index.js
@@ -0,0 +1,13 @@
+let initialValue = 1
+let reverseValue = 5
+
+while(initialValue<=10){
+    if(initialValue<=5){
+        console.log(initialValue)
+    }
+    else{
+        console.log(reverseValue)
+        reverseValue--
+    }
+    initialValue++
+}
